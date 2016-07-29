b899951deecbab3093621d367ce1e494eeefb306
zo0z <azzaea@uofk.edu>
2016-07-29 00:50:53 -0500

    GIAB manipulation files

---
+++ .BatchJobs.R
@@ -0,0 +1,7 @@
+cluster.functions = makeClusterFunctionsTorque("/home/groups/hpcbio_shared/azza/GIAB/src/simple.tmpl")
+mail.start = "aeahmed@illinois.edu"
+mail.done = "aeahmed@illinois.edu"
+mail.error = "aeahmed@illinois.edu"
+db.driver = "SQLite"
+db.options = list()
+debug = FALSE
